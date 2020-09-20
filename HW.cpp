4f67fe31c083b99fa4550fd54e78d433f399d6f1
DESKTOP-67FUCJE\Himaswetha KS <Himaswetha.Kurnool-Shamarao.Partner@HANECS.de>
2020-09-20 19:54:27 +0530

    cc

--- HW.cpp
+++ HW.cpp
@@ -4,4 +4,5 @@ int main()
 {
     cout<<"HELLO";
     cout<<"World";
+    cout<<"GIT";
 }
---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"HelloWorld";
+<<<<<<< HEAD
+<<<<<<< HEAD
+    int a;
+    cout<<""Master branch code";
+    cout<<"Create new branch";
+=======
+    int a;
+    cout<<""Master branch code";
+    int a =10;
+    cout<<"Create new branch";
+    cout<<"New";
+>>>>>>> test1_branch
+}
