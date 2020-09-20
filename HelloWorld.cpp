9a93e284e69c106043b70fb3c1d9eb05f3bfbb77
DESKTOP-67FUCJE\Himaswetha KS <Himaswetha.Kurnool-Shamarao.Partner@HANECS.de>
2020-09-20 19:13:04 +0530

    computing sum

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main()
 {
     cout<<"HelloWorld";
-    int a;
+    int a =10;
     cout<<"Create new branch";
+    int b = 20, sum = 0;
+    sum = a + b;
+    cout<<sum<<endl;
 }
